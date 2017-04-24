a25a76c1486e488e4beb1ade541e052a86982a7d
Denews <gkvms963@naver.com>
2017-04-24 21:45:08 +0900

    Sound쪽 완성

--- client/src/qss/nodoubt/sounds/Sound.java
+++ client/src/qss/nodoubt/sounds/Sound.java
@@ -3,11 +3,17 @@ package qss.nodoubt.sounds;
 import static org.lwjgl.openal.AL10.*;
 
 import java.nio.*;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.lwjgl.BufferUtils;
 import org.lwjgl.openal.*;
+
+import qss.nodoubt.utils.FileUtils;
 
 public class Sound {
 	private static Sound s_Instance = null;
+	private Map<String, SoundSource> m_Sources = new HashMap<String, SoundSource>();
 	private long m_SoundDevice;
 	private long m_Context;
 	
@@ -32,5 +38,37 @@ public class Sound {
 	public void shutdown() {
 		ALC10.alcDestroyContext(m_Context);
 		ALC10.alcCloseDevice(m_SoundDevice);
+	}
+	
+	public void loadSound(String name, String path){
+		int buffer = alGenBuffers();
+		IntBuffer channels = BufferUtils.createIntBuffer(1);
+		IntBuffer sample_rate = BufferUtils.createIntBuffer(1);
+		ShortBuffer data = FileUtils.loadSoundFile(path, channels, sample_rate);
+		
+		if(channels.get(0) == 1) {
+			alBufferData(buffer, AL_FORMAT_MONO16, data, sample_rate.get(0));
+		}else if(channels.get(0) == 2) {
+			alBufferData(buffer, AL_FORMAT_STEREO16, data, sample_rate.get(0));
+		}
+		
+		SoundSource source = new SoundSource(buffer);
+		m_Sources.put(name, source);
+	}
+	
+	public void play(String name) {
+		m_Sources.get(name).play();
+	}
+	
+	public void pause(String name) {
+		m_Sources.get(name).pause();
+	}
+	
+	public void stop(String name) {
+		m_Sources.get(name).stop();
+	}
+	
+	public void setLoop(String name, boolean isLoop) {
+		m_Sources.get(name).setLoop(isLoop);
 	}
 }
---
+++ client/src/qss/nodoubt/sounds/SoundSource.java
@@ -0,0 +1,47 @@
+package qss.nodoubt.sounds;
+
+import static org.lwjgl.openal.AL10.*;
+
+public class SoundSource {
+	private int m_Source;
+	private int m_Buffer;
+	
+	public SoundSource() {
+		m_Source = alGenSources();
+		alSourcef(m_Source, AL_GAIN, 1);
+		alSourcef(m_Source, AL_PITCH, 1);
+		alSource3f(m_Source, AL_POSITION, 0, 0, 0);
+	}
+	
+	public SoundSource(int buffer) {
+		m_Source = alGenSources();
+		alSourcef(m_Source, AL_GAIN, 1);
+		alSourcef(m_Source, AL_PITCH, 1);
+		alSource3f(m_Source, AL_POSITION, 0, 0, 0);
+		m_Buffer = buffer;
+	}
+	
+	public void setBuffer(int buffer) {
+		m_Buffer = buffer;
+	}
+	
+	void play() {
+		alSourcei(m_Source, AL_BUFFER, m_Buffer);
+		alSourcePlay(m_Source);
+	}
+	
+	void setLoop(boolean isLoop) {
+		if(isLoop)
+			alSourcei(m_Source, AL_LOOPING, AL_TRUE);
+		else
+			alSourcei(m_Source, AL_LOOPING, AL_FALSE);
+	}
+	
+	void pause() {
+		alSourcePause(m_Source);
+	}
+	
+	void stop() {
+		alSourceStop(m_Source);
+	}
+}
