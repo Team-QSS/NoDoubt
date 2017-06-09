556371d52f2c12cdb7c8451871dc13cd1263c918
Splash <kkfenix3085@naver.com>
2017-06-09 19:43:41 +0900

    인게임 시퀀스 추가

--- client/src/qss/nodoubt/graphics/Graphic.java
+++ client/src/qss/nodoubt/graphics/Graphic.java
@@ -29,7 +29,7 @@ public class Graphic {
 	 */
 	public void beginDraw() {
 		glClear(GL_COLOR_BUFFER_BIT);
-		glClearColor(0.0f, 1.0f, 1.0f, 0.0f);
+		glClearColor(0.0f, 0.0f, 1.0f, 0.0f);
 	}
 	
 	/**
