5b69ea8dd7670962c263a08398d81858e5aec5bc
BAE Yong Ho <byh020907@naver.com>
2017-07-02 11:50:12 +0900

    서버 프로토콜 상수로 정의

--- server/src/qss/nodoubt/util/Util.java
+++ server/src/qss/nodoubt/util/Util.java
@@ -3,6 +3,8 @@ package qss.nodoubt.util;
 
 import javax.swing.JTextArea;
 
+import org.json.simple.JSONObject;
+
 public class Util {
 	
 	//모든 서버상의 로그는 이함수들을 이용하여 출력
@@ -16,6 +18,15 @@ public class Util {
 	public static void printDebugLog(boolean DEBUG_MODE,JTextArea mainTextArea,Object content){
 		if(DEBUG_MODE)
 		printLog(mainTextArea,content);
+	}
+	
+	public static JSONObject packetGenerator(String protocol,KeyValue ...kvs){
+		JSONObject data=new JSONObject();
+		data.put("Protocol", protocol);
+		for(KeyValue kv:kvs){
+			data.put(kv.key, kv.value);
+		}
+		return data;
 	}
 	
 }
