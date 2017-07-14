d1756d253b6b3d09601367de62da8204ec7aa1f0
DSMDongly <gdh0608@naver.com>
2017-07-14 19:37:59 +0900

    로그인 기능 구현, Gson 멤버 변수 추가
    1. UserService 클래스 내에서 로그인 기능을 구현하였고, JSONObject 객체 역직렬화를 위해 Gson 멤버 변수를
    추가하였다.
    
    2. 회원가입 시 사용자의 이름을 저장할 지 저장하지 않을 지 구조적 논의가 필요하다

--- server/src/qss/nodoubt/room/UserService.java
+++ server/src/qss/nodoubt/room/UserService.java
@@ -1,12 +1,17 @@
 package qss.nodoubt.room;
 
 import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import com.google.gson.Gson;
 
 import qss.nodoubt.database.Database;
 
 public class UserService {
     private static UserService instance;
+    
     private Database database = Database.getInstance();
+    private Gson gson = new Gson();
     
     private UserService(){}
     
@@ -25,5 +30,18 @@ public class UserService {
     	// 로그인을 위해 id, password를 이용해서 사용자 조회
     	String sql = "SELECT * FROM users WHERE id=? && password=?";
     	return database.executeAndGet(sql, id, password);
+    }
+    
+    public User login(String id, String password) {
+    	// 사용자가 입력한 아이디, 패스워드를 이용한 로그인을 수행 (id: 사용자의 아이디, password: 사용자의 비밀번호)
+    	
+    	if (users.size() == 1) {
+    	    
+    	} else {
+    	}
+    }
+    
+    public User login(User user) {
+    	// 아이디, 비밀번호가 담긴 User 객체를 이용해서 로그인 수행
     }
 }
