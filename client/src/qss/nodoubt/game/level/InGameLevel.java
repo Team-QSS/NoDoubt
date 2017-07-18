5eb05309595d30741e588c13989fbe40a8ae95f7
Denews <gkvms963@naver.com>
2017-07-18 20:12:34 +0900

    충돌 해결

--- client/src/qss/nodoubt/game/level/InGameLevel.java
+++ client/src/qss/nodoubt/game/level/InGameLevel.java
@@ -8,13 +8,8 @@ public class InGameLevel extends GameLevel{
 		IDLE, SETORDER, ROLLDICE, CALLDOUBT, STEPOTHER, PROCESSITEM, 
 	}
 	public InGameLevel() {
-<<<<<<< HEAD
 		addObject(new Background("InGameBackground"));
 		addObject(new Label());
-=======
-		m_GameField = new Background("Test");
-		addObject(m_GameField);
->>>>>>> refs/remotes/origin/master
 	}
 
 	@Override
