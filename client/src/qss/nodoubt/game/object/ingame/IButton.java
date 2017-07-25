91cd384410b9a9c26858f6ec24fec07e0b2d98ad
Denews <gkvms963@naver.com>
2017-07-25 14:20:02 +0900

    버튼 포커싱 추가

--- client/src/qss/nodoubt/game/object/ingame/IButton.java
+++ client/src/qss/nodoubt/game/object/ingame/IButton.java
@@ -1,8 +1,11 @@
 package qss.nodoubt.game.object.ingame;
 
 import qss.nodoubt.game.object.GameObject;
+import qss.nodoubt.input.Input;
 
 import static org.lwjgl.glfw.GLFW.*;
+
+import org.joml.Vector2f;
 
 public class IButton extends GameObject{
 	private interface ClickListener {
@@ -10,26 +13,35 @@ public class IButton extends GameObject{
 	}
 	
 	private ClickListener m_Listener;
+	
+	private String m_TextureName;
 
 	public IButton(int n, ClickListener listener) {
 		super("IButton" + n, 1);
+		m_Listener = listener;
+		m_TextureName = "IButton" + n;
+		
 		setPosition(-777, 405 - (n - 1) * 135);
 		setEventListener(null, (action, button) -> {
 			if(isMouseOnButton()) {
 				if(action == GLFW_PRESS) {
 					focus();
 				}else if(action == GLFW_RELEASE) {
-					unfocus();
 					click();
 				}
 			}
+			if(action == GLFW_RELEASE) {
+				unfocus();
+			}
 		});
 		
-		m_Listener = listener;
+		
 	}
 	
 	public IButton(String type, ClickListener listener) {
 		super("IButton" + type, 1);
+		m_Listener = listener;
+		m_TextureName = "IButton" + type;
 		if(type.equals("Doubt")) {
 			setPosition(-777, -405);
 		}else if(type.equals("Roll")) {
@@ -41,13 +53,13 @@ public class IButton extends GameObject{
 				if(action == GLFW_PRESS) {
 					focus();
 				}else if(action == GLFW_RELEASE) {
-					unfocus();
 					click();
 				}
 			}
+			if(action == GLFW_RELEASE) {
+				unfocus();
+			}
 		});
-		
-		m_Listener = listener;
 	}
 
 	@Override
@@ -57,15 +69,23 @@ public class IButton extends GameObject{
 	}
 	
 	private boolean isMouseOnButton() {
-		return true;
+		Vector2f mousePosition = Input.getInstance().getCursorPosition();
+		Vector2f position = getPosition();
+		
+		if(Math.abs(mousePosition.x - position.x) <= 111) {
+			if(Math.abs(mousePosition.y - position.y) <= 83) {
+				return true;
+			}
+		}
+		return false;
 	}
 	
 	private void focus() {
-		
+		setTexture(m_TextureName + "F");
 	}
 	
 	private void unfocus() {
-		
+		setTexture(m_TextureName);
 	}
 	
 	private void click() {
