b8a9c345bda6d7a797858ca8806e3a7acdca200a
Denews <gkvms963@naver.com>
2017-08-30 22:23:20 +0900

    TabPanel 추가

---
+++ client/src/qss/nodoubt/game/object/ingame/TabPanel.java
@@ -0,0 +1,18 @@
+package qss.nodoubt.game.object.ingame;
+
+import qss.nodoubt.game.object.GameObject;
+
+public class TabPanel extends GameObject{
+
+	public TabPanel() {
+		super("TabPanel", 0);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void update(float deltaTime) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
