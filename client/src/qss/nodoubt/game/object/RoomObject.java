a6e8b8a6de62e0ab478f52c328b3127771503413
Splash <kkfenix3085@naver.com>
2017-08-31 23:41:03 +0900

    RoomObject 생성 테스트 성공

--- client/src/qss/nodoubt/game/object/RoomObject.java
+++ client/src/qss/nodoubt/game/object/RoomObject.java
@@ -8,20 +8,21 @@ public class RoomObject extends GameObject {
 	public TextBox m_Owner;
 	public TextBox m_Players;
 	
-	private static final float GAMENAMETEXT = 227;
-	private static final float OWNERTEXT = 941;
-	private static final float PLAYERSTEXT = 1574;
+	private static final float GAMENAMETEXT = -812;
+	private static final float OWNERTEXT = -123;
+	private static final float PLAYERSTEXT = 614;
+	private static final float TEXTY = 154;
 	
 	private static final float GAMENAMEX = 467;
 	private static final float OWNERX = 1137;
 	private static final float PLAYERSX = 1627;
-
+	
 	private static final float Y = 410;
-	private static final float TEXTY = 386;
 	
-	private static final Vector3f COLOR = new Vector3f(0x9a, 0x6f, 0x52);
 	
-	public RoomObject(float depth, float index, String gamename, String owner, int players) {
+	private static final Vector3f COLOR = new Vector3f(0x9a/255f, 0x6f/255f, 0x52/255f);
+	
+	public RoomObject(float depth, int index, String gamename, String owner, int players) {
 		super("Blank", depth);
 		
 		m_GameName = new TextBox(0, GAMENAMEX, Y - 110 * index, 714, GAMENAMETEXT, TEXTY - 110 * index, false, null, COLOR);
@@ -39,6 +40,7 @@ public class RoomObject extends GameObject {
 		m_GameName.update(deltaTime);
 		m_Owner.update(deltaTime);
 		m_Players.update(deltaTime);
+		System.out.println("방 정보 출력");
 	}
 
 }
