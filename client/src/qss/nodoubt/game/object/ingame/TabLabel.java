488a767cf4db8445a1308f9163415939ebca921c
Denews <gkvms963@naver.com>
2017-08-31 22:17:36 +0900

    TabLabel추가, 임시로 색상에 따라 위치가 결정되도록 함.

--- client/src/qss/nodoubt/game/object/ingame/TabLabel.java
+++ client/src/qss/nodoubt/game/object/ingame/TabLabel.java
@@ -1,18 +1,40 @@
 package qss.nodoubt.game.object.ingame;
 
+import org.joml.Vector2f;
+import org.joml.Vector3f;
+
 import qss.nodoubt.game.object.GameObject;
 
 public class TabLabel extends GameObject{
+	private static final Vector3f[] m_Colors = new Vector3f[]{
+			new Vector3f(0xff / 255f, 0x39 / 255f, 0x39 / 255f), 
+			new Vector3f(0x23 / 255f, 0x75 / 255f, 0xeb / 255f), 
+			new Vector3f(0x36 / 255f, 0x8c / 255f, 0x49 / 255f), 
+			new Vector3f(0xff / 255f, 0xf4 / 255f, 0x1f / 255f), 
+			new Vector3f(0xff / 255f, 0xff / 255f, 0xff / 255f), 
+			new Vector3f(0xa9 / 255f, 0x24 / 255f, 0xff / 255f)
+	};
+	
+	private String m_Name;
+	private int m_Color;
 
 	public TabLabel(String name, char color) {
 		super("TabDot" + color, 8);
-		// TODO Auto-generated constructor stub
+		
+		switch(color)
+		{
+		case 'R': setPosition(new Vector2f(-888, -64)); m_Color = 0; break;
+		case 'B': setPosition(new Vector2f(-888, -150)); m_Color = 1; break;
+		case 'G': setPosition(new Vector2f(-888, -236)); m_Color = 2; break;
+		case 'Y': setPosition(new Vector2f(-888, -322)); m_Color = 3; break;
+		case 'W': setPosition(new Vector2f(-888, -408)); m_Color = 4; break;
+		case 'P': setPosition(new Vector2f(-888, -494)); m_Color = 5; break;
+		}
 	}
 
 	@Override
 	public void update(float deltaTime) {
-		// TODO Auto-generated method stub
-		
+		drawTextCall("FontR7", m_Name, new Vector2f(getPosition()).add(new Vector2f(77, 16)), m_Colors[m_Color]);
 	}
 
 }
