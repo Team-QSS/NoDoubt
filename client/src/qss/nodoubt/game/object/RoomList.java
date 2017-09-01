5011de38b4412b5fee31788c71057a23c6bc49c0
Splash <kkfenix3085@naver.com>
2017-09-01 23:53:50 +0900

    RoomList 출력하기

--- client/src/qss/nodoubt/game/object/RoomList.java
+++ client/src/qss/nodoubt/game/object/RoomList.java
@@ -4,7 +4,7 @@ import java.util.LinkedList;
 
 public class RoomList extends GameObject{
 	
-	private LinkedList<RoomObject> RoomList = new LinkedList<RoomObject>();
+	private LinkedList<RoomObject> roomList = new LinkedList<RoomObject>();
 	
 	public RoomList() {
 		super("Blank", 0);
@@ -12,28 +12,32 @@ public class RoomList extends GameObject{
 
 	@Override
 	public void update(float deltaTime) {
-		for(RoomObject R : RoomList){
+		for(RoomObject R : roomList){
 			R.update(deltaTime);
 		}
 	}
 	
 	//구현중
 	public void addRoomObject(RoomObject roomObj){
-		roomObj.setIndex(RoomList.size());
-		RoomList.add(roomObj);
+		roomObj.setIndex(roomList.size());
+		roomList.add(roomObj);
 	}
 	
 	public void removeRoomObject(int index){
-		RoomList.remove(index);
+		roomList.remove(index);
 		
-		for(int i=index;i<RoomList.size();i++){
-			RoomList.get(i).setIndex(i);
+		for(int i=index;i<roomList.size();i++){
+			roomList.get(i).setIndex(i);
 		}
 	}
 	
-	public void addRoomObject(int index,RoomObject roomObj){
+	public void addRoomObject(int index, RoomObject roomObj){
 		roomObj.setIndex(index);
-		RoomList.add(index,roomObj);
+		roomList.add(index,roomObj);
+	}
+	
+	public RoomObject getIndex(int index){
+		return roomList.get(index);
 	}
 
 }
