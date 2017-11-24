178b14843a0371029b885e4856bf34679ba77605
DSMdongly <gdh0608@naver.com>
2017-11-24 19:19:42 +0900

    Headless Server에StartGameRequest와  SetPlayingReport 프로토콜, 이에 관련된 처리 로직 추가 및 배포 완료

--- headless-server/src/qss/nodoubt/Server.java
+++ headless-server/src/qss/nodoubt/Server.java
@@ -453,12 +453,22 @@ public class Server {
 					Room room=user.getCurrentRoom();
 					room.setMaster(null);
 					
-					sendData=Util.packetGenerator(Protocol.START_GAME_REPORT);
+					user.getCurrentRoom().setPlaying(true);
+					
+					sendData=Util.packetGenerator(Protocol.SET_ROOM_PLAYING, new KeyValue("RoomID",user.getCurrentRoomId()));
 					
 					//자신의 유저와 같은방에있는 애들에게 보냄
 					send(sendData,c->{
 						User u=c.getCurrentUser();
 						return u!=null && u.isOnline()&&u.getCurrentRoomId()==user.getCurrentRoomId();
+					});
+					
+					sendData=Util.packetGenerator(Protocol.SET_ROOM_PLAYING, new KeyValue("RoomID",user.getCurrentRoomId()));
+					
+					//로비로 보냄
+					send(sendData,c->{
+					    User u=c.getCurrentUser();
+					    return u!=null&&!user.equals(u)&&u.isOnline()&&u.getCurrentRoomId()==RoomManager.LOBBY;
 					});
 				}break;
 				
